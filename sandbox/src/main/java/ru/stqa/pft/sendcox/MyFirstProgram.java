c464e50d9b7244cc47d0bf22ece3fee62a264337
MOON\grebenkin <grebenkin@proitr.ru>
2018-07-09 10:10:33 +0500

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sendcox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sendcox/MyFirstProgram.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sendcox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args) {
-System.out.println("Hrllo, world!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hrllo, world!");
+  }
 }
